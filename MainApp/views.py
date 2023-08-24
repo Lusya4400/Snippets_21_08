833499929ee85aba2beb608e943141ed58334a7a
usasheva_ludmila <luda-a@rambler.ru>
2023-08-24 23:24:26 +0300

    lesson 9

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,14 +1,29 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
-from MainApp.forms import SnippetForm
-
+from MainApp.forms import SnippetForm, UserRegistrationForm
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
     return render(request, 'pages/index.html', context)
 
+
+@login_required
+def my_snippets(request):
+    snippets = Snippet.objects.filter(user=request.user)
+    context = {
+        'pagename': 'Мои сниппеты',
+        'snippets': snippets
+        }
+    return render(request, 'pages/view_snippets.html', context)
+
+
+@login_required
 def add_snippet_page(request):
+    # Получаем пустую форму
     if request.method == "GET":      
         form = SnippetForm()
         context = {
@@ -16,29 +31,103 @@ def add_snippet_page(request):
             'form': form
         }
         return render(request, 'pages/add_snippet.html', context)
+ 
+    # Записываем новый снипет из данных формы
     if request.method == "POST":
-        print(f'{request.POST = }')
         form = SnippetForm(request.POST)
         if form.is_valid():
-            form.save()
+            snippet = form.save(commit=False)
+            if request.user.is_authenticated:
+                snippet.user = request.user
+                snippet.save()
             return redirect("snippets-list")
         return render(request, 'pages/add_snippet.html', {'form': form})
 
 def snippets_page(request):
-    snippets = Snippet.objects.all()
-    len_s = len(snippets)
-    context = {'pagename': 'Просмотр сниппетов',
-            'snippets': snippets,
-            'num' : len_s
+    snippets = Snippet.objects.filter(public=True)
+    context = {
+        'pagename': 'Просмотр сниппетов',
+        'snippets': snippets,
     }
     return render(request, 'pages/view_snippets.html', context)
 
 def snippet_detail(request, snippet_id):
-    snippet = Snippet.objects.get(id=snippet_id)
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        raise Http404
     context ={'pagename': 'Просмотр сниппета',
             'snippet': snippet,
+            'type': 'view'
     }
     return render(request, 'pages/snippet_detail.html', context)
+
+def snippet_delete(request, snippet_id):
+    snippet = Snippet.objects.get(id=snippet_id)
+    snippet.delete()
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+
+def snippet_edit(request, snippet_id):
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        raise Http404
+    # получаем данные сниппера
+    if request.method == "GET":      
+        context = {
+            'pagename': 'Редактирование сниппета',
+            'snippet': snippet,
+            'type': "edit"
+        }
+        return render(request, 'pages/snippet_detail.html', context)
+    
+    #Новый сниппет
+    if request.method == "POST":
+        data_form = request.POST
+        snippet.name = data_form["name"]
+        snippet.lang = data_form["lang"]
+        snippet.code = data_form["code"]
+        snippet.creation_date = data_form["creation_date"]
+        snippet.public = data_form.get("public", False)
+        snippet.save()
+        return redirect("snippets-list")
+
+def create_user(request):
+    context = {"pagename": "Регистрация пользователя"}
+    if request.method == "GET":
+        form = UserRegistrationForm()
+        context["form"] = form
+        return render(request, "pages/registration.html", context)
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        context['form'] = form
+        return render(request, "pages/registration.html", context)
+
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+        else:
+            context = {
+                'pagename': 'PythonBin',
+                'errors': ['wrong username or password']
+            }
+            return render(request, 'pages/index.html', context)
+    return redirect('home')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect(request.META.get("HTTP_REFERER", '/'))
+
 
 # def create_snippet(request):
 #     if request.method == "POST":
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -4,9 +4,15 @@ from django.conf import settings
 from MainApp import views
 
 urlpatterns = [
-    path('', views.index_page, name='index'),
+    path('', views.index_page, name='home'),
     path('snippets/add', views.add_snippet_page, name='snippet-add'),
     path('snippets/list', views.snippets_page, name='snippets-list'),
     path('snippet/<int:snippet_id>', views.snippet_detail, name='snippet_detail'),
+    path('snippet/<int:snippet_id>/delete', views.snippet_delete, name='snippet_delete'),
+    path('snippet/<int:snippet_id>/edit', views.snippet_edit, name='snippet-edit'),
+    path('snippet/my', views.my_snippets, name='my-snippets'),
+    path('auth/register', views.create_user, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 #    path('snippet/create', views.create_snippet, name='create-snippet'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
